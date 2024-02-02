107aacee5606252f7fc073deb66f90b91cfe3e49
b-hayes <bradley.hayes@yourgiving.co>
2024-02-02 16:35:47 +0700

    Scrolling changes the grid scale

--- public/js/Grid.js
+++ public/js/Grid.js
@@ -5,6 +5,20 @@ export default class Grid extends Component {
     constructor({scale = 1}) {
         super()
         this.scale = scale
+
+        //apply scale changes when scrolling
+        window.addEventListener('wheel', (e) => {
+            e.preventDefault()
+            this.scale += e.deltaY * -0.001
+            this.scale = Math.max(0.001, Math.min(10, this.scale))
+            //this.element().style.transform = `scale(${this.scale})`
+
+            // Snap scale change to the nearest 0.001
+            this.scale = Math.round(this.scale * 10000) / 10000;
+
+            //update scale tooltip
+            this.element().getElementsByClassName('tool-tip')[0].innerText = `Scale: 1px = ${this.scale}m`
+        })
     }
 
     html() {
@@ -20,6 +34,7 @@ export default class Grid extends Component {
 const html = `
     <div class="grid">
         <div class="grid-background">
+            <div class="tool-tip">Scale: 1px = .01m</div>
             <slot></slot>
         </div>
     </div>
@@ -37,6 +52,7 @@ const style = `
         overflow: hidden;
         box-shadow: inset 5px 5px 10px 3px rgba(0, 0, 0, 0.5);
     }
+
     .grid-background {
         background-image: repeating-linear-gradient(var(--foreground) 0 1px, transparent 1px 100%),
         repeating-linear-gradient(90deg, var(--foreground) 0 1px, transparent 1px 100%);
@@ -44,5 +60,17 @@ const style = `
         width: 100%;
         height: 100%;
         opacity: 0.5;
+    }
+
+    .grid-background .tool-tip {
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: white;
+        background-color: var(--foreground, black);
+        opacity: 0.8;
+        padding: 5px;
+        font-size: 1.5em;
+        z-index: 100;
     }
 `
