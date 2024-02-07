9e70f6010ef3319e27eec0a381d8f608529caf9b
b-hayes <bradley.hayes@yourgiving.co>
2024-02-07 22:50:25 +0700

    placholder

--- views/feedback.php
+++ views/feedback.php
@@ -9,6 +9,12 @@ declare(strict_types=1);
     <title>Feedback for Room Planner</title>
 </head>
 <body>
+<div class="centered">
+    <div class="middle" style="width: 100%;text-align: center;">
+        <h1>Feedback for Room Planner</h1>
+        <p>Sorry, this feature is not yet implemented.</p>
+    </div>
+</div>
 
 </body>
 </html>
