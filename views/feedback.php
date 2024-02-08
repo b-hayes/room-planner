2fa7906e6ea11c30065a5e46ea32da054cb6e432
b-hayes <bradley.hayes@yourgiving.co>
2024-02-09 00:21:53 +0700

    WIP useing phpmailer need to setup smtp

--- views/feedback.php
+++ views/feedback.php
@@ -1,19 +1,40 @@
 <?php
 declare(strict_types=1);
 
-// Email dev with the feedback
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'] ?? 'Anonymous';
-    $feedback = $_POST['feedback'];
-    $to = getenv('feedbackEmail');
-    $subject = 'Feedback for Room Planner';
-    $message = "Feedback from: $email\n\n$feedback";
-    $sent = mail($to, $subject, $message);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
-    //display a message if the mail was successful
-    if (!$sent) {
-        throw new \Exception('Failed to send feedback.');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Email feedback to the devs
+    $feedback = $_POST['feedback'];
+
+    $mail = new PHPMailer(true);
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = getenv('SMTP_HOST');                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = getenv('SMTP_USER');                     //SMTP username
+    $mail->Password = getenv('SMTP_PASSWORD');                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $userName = $_POST['name'] ?? 'Anonymous';
+    $userEmail = $_POST['email'] ?? null;
+    if ($userEmail) {
+        $mail->setFrom($userEmail, $userName);
     }
+    $mail->addAddress(getenv('feedbackEmail'));
+
+    //Content
+    $mail->isHTML();
+    $mail->Subject = 'Room Planer feedback.';
+    $mail->Body = "<b>$userName</b> submitted some feedback: <p>$feedback</p>";
+    $mail->AltBody = "$userName submitted some feedback: \n\n $feedback";
+
+    $mail->send();
 
     echo '<h1>Thank you for your feedback!</h1>';
     //log the feedback
@@ -60,16 +81,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         max-width: 100%;
         max-height: 100%;
     }
+
     textarea {
         width: 600px;
         height: 200px;
     }
+
     input {
         width: 300px;
     }
+
     label {
         width: 200px;
     }
+
     button {
         width: 200px;
         height: 50px;
