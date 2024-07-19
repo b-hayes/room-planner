a6d6c34b73290e7ab4854381846c5bfdaf74458b
b-hayes <bradozman@gmail.com>
2024-07-19 20:42:42 +1000

    notes

--- public/js/TestBind2.js
+++ public/js/TestBind2.js
@@ -4,6 +4,10 @@ export default class TestBind2 extends Component {
 
     count = 0
 
+    // in this example, the loader will add a proxy to this component so that
+    // any time a property is set, the component will be reloaded.
+    // this is not ideal, but it's a start.
+
     html() {
         return `
     <div for="">
