75be1f3d339a245672bb32913b41a0d80edb867b
b-hayes <30540495+b-hayes@users.noreply.github.com>
2024-07-19 22:21:09 +1000

    Revert "Improvments to scroll and zoom + Data binding prototype added" (#11)

--- public/js/Grid.js
+++ public/js/Grid.js
@@ -1,6 +1,5 @@
 import Component from "./Scafold/Component.js"
 import Vector from "./Vector.js"
-import Shape from "./Shape.js"
 
 export default class Grid extends Component {
 
@@ -9,47 +8,9 @@ export default class Grid extends Component {
         this.scale = scale
         this.background = this.element() //the background is the element now.
         this.toolTip = this.element().getElementsByClassName('tool-tip')[0]
+
+        document.addEventListener('wheel', (e) => this.scroll(e), false)
         document.addEventListener('mousedown', (e) => this.mouseDown(e), false)
-        document.addEventListener('wheel', (e) => this.scroll(e), {passive: false})
-        document.addEventListener('scroll', (e) => this.scroll(e), {passive: false})
-
-        //prevent the browser from scrolling when control is held and account for Chrome not letting us capture the first ctrl+scroll event
-        let handlingScroll = false;
-        window.addEventListener('keydown', function (event) {
-            if ((event.ctrlKey === true || event.metaKey === true) && handlingScroll === true) {
-                event.preventDefault();
-            }
-        }, {passive: false});
-
-        window.addEventListener('wheel', function (event) {
-            if (event.ctrlKey === true || event.metaKey === true) {
-                handlingScroll = true
-                //set timeout to reset the handlingScroll variable after the scroll event is finished
-                setTimeout(() => {
-                    handlingScroll = false
-                }, 10)
-                event.preventDefault();
-            } else {
-                handlingScroll = false
-            }
-        }, {passive: false});
-
-        document.body.onmousedown = function(e) {
-            if(e.button == 1) {
-                e.preventDefault();
-                return false;
-            }
-        }
-    }
-
-
-    //TODO: update usages of grids and shapes to use the new addShape method and remove the old method of shapes needing a parent injected
-    addShape(shape) {
-        this.element().appendChild(shape.element())
-    }
-
-    shapes() {
-        return Array.from(this.element().children).filter(child => child instanceof Shape)
     }
 
     mouseDown(e) {
@@ -106,29 +67,13 @@ export default class Grid extends Component {
         }
     }
 
-    preventDefaultBehaviour(e) {
-        e.preventDefault();
-    }
-
     scroll(e) {
-        if (!this.element().contains(e.target)) {
-            return
-        }
-
-        //ZOOM in and out
-        if (e.ctrlKey || e.metaKey) {
+        //if ctrl or command is held, then scroll to zoom
+        if (this.element().contains(e.target)) {
             let maxScale = 5
             let minScale = 0.25
             let toolTip = this.toolTip
-            let scaleShift = e.deltaY * -0.001
-
-            //on windows using my scroll wheel the scaleShift is always 0.1 which isn't a smooth zoom amount
-            // so lets change it to .01 for a smoother zoom and more control
-            if (Math.abs(scaleShift) === 0.1 ) {
-                scaleShift = scaleShift / 10
-            }
-
-            if (this.scale + scaleShift > maxScale || this.scale + scaleShift < minScale) {
+            if (this.scale + e.deltaY * -0.001 > maxScale || this.scale + e.deltaY * -0.001 < minScale) {
                 //make the tooltip flash red to indicate the scale is at its limit
                 //get the original color,  if not already red
                 if (toolTip.style.color !== 'red') {
@@ -142,12 +87,11 @@ export default class Grid extends Component {
                     , 100)
                 return
             }
-
-            this.scale += scaleShift
+            //e.preventDefault()
+            this.scale += e.deltaY * -0.001
             this.scale = Math.max(minScale, Math.min(maxScale, this.scale))
             this.scale = Math.round(this.scale * 10000) / 10000;
             toolTip.innerText = `Scale: 1px = ${this.scale}cm`
-
             this.background.style.backgroundSize = `${100 * this.scale}px ${100 * this.scale}px`
             let event = new CustomEvent('grid-scale-changed', {
                 detail: {
@@ -158,83 +102,10 @@ export default class Grid extends Component {
                 }
             })
             this.dispatchEventWithDebounce(event, 0)
-        } else {
-            e.preventDefault()
-            //adjust the background offset to match the scroll position
-            let shift =  new Vector(e.deltaX, e.deltaY)
-            if (e.shiftKey === true) {
-                shift = new Vector(e.deltaY, e.deltaX)//change the direction of the scroll if shift is held
-            }
-
-            let changingDirection = this.lastShift && this.lastShift.x * shift.x < 0 || this.lastShift && this.lastShift.y * shift.y < 0;
-
-            //if scroll target exists then add the shift to it, otherwise create it
-            if (this.scrollTarget && !changingDirection) {
-                this.scrollTarget.x += shift.x
-                this.scrollTarget.y += shift.y
-            } else {
-                this.scrollTarget = new Vector(
-                    this.element().scrollLeft + shift.x,
-                    this.element().scrollTop + shift.y
-                )
-            }
-
-            //clamp the scroll target to the bounds of the scroll area
-            this.scrollTarget.clamp(
-                0,
-                this.element().scrollWidth - this.element().clientWidth,
-                0,
-                this.element().scrollHeight - this.element().clientHeight
-            )
-
-            //if shift is less than 100 then just scroll to the target
-            if (Math.abs(shift.x) < 100 && Math.abs(shift.y) < 100) {
-                this.element().scrollTo(this.scrollTarget.x, this.scrollTarget.y)
-                this.background.style.backgroundPosition = `-${this.scrollTarget.x}px -${this.scrollTarget.y}px`
-                return
-            }
-
-            this.scrollToSmoothly(this.scrollTarget.x, this.scrollTarget.y, 150)
-            this.lastShift = shift
-        }
-    }
-
-    scrollToSmoothly(targetX, targetY, duration) {
-        let startX = this.element().scrollLeft;
-        let startY = this.element().scrollTop;
-        let diffX = targetX - startX;
-        let diffY = targetY - startY;
-        let startTime = null;
-
-        //if animation exists cancel it
-        if (this.animationFrameId) {
-            window.cancelAnimationFrame(this.animationFrameId)
         }
 
-        let animation = (currentTime) => {
-            if (startTime === null) startTime = currentTime;
-            let progress = Math.min((currentTime - startTime) / duration, 1)
-
-            let scroll = new Vector(
-                startX + diffX * progress,
-                startY + diffY * progress
-            )
-
-            scroll.clamp(
-                0,
-                this.element().scrollWidth - this.element().clientWidth,
-                0,
-                this.element().scrollHeight - this.element().clientHeight
-            )
-
-            this.background.style.backgroundPosition = `-${scroll.x}px -${scroll.y}px`
-            this.element().scrollTo(scroll.x, scroll.y);
-            if (progress < 1) {
-                this.animationFrameId = window.requestAnimationFrame(animation);
-            }
-        };
-
-        this.animationFrameId = window.requestAnimationFrame(animation);
+        //update the background offset to match the scroll change, inverted
+        this.background.style.backgroundPosition = `-${this.element().scrollLeft}px -${this.element().scrollTop}px`
     }
 
     html() {
@@ -262,10 +133,7 @@ const style = `
         min-height: 100%;
         min-width: 100%;
         box-shadow: inset 5px 5px 10px 3px rgba(0, 0, 0, 0.5);
-        overflow: auto;
-        /* set the scrollbars to be the same colour as the background and be thin  */
-        scrollbar-width: thin;
-        scrollbar-color: var(--grid-background) var(--grid-foreground);
+        overflow: hidden;
     }
 
     .grid-background {
--- public/js/Scafold/Loader.js
+++ public/js/Scafold/Loader.js
@@ -1,34 +1,6 @@
 import Component from "./Component.js"
 
 export default class Loader {
-
-    static components = {}
-
-    static addComponent(component) {
-        // create a proxy to update the html when any property changes
-        let handler = {
-            set: (target, property, value) => {
-                target[property] = value
-                console.log('set via proxy', property, value)
-                Loader.reloadComponent(target)
-                return true
-            }
-        }
-        Loader.components[component._instanceId] = new Proxy(component, handler)
-
-        // Add a reference to the Loader class to the window.
-        if (!window.Loader) window.Loader = Loader
-        //todo: give loader cool name? Modulatte?
-    }
-
-    static getComponent(id) {
-        return Loader.components[id]
-    }
-
-    static removeComponent(id) {
-        delete Loader.components[id]
-    }
-
     static loadStyles(style, styleId) {
         if (!style) {
             return
@@ -44,32 +16,10 @@ export default class Loader {
         document.head.appendChild(styleElement)
     }
 
-    static loadHtml(html, data = {}) {
-        let processedHtml = html;
-        for (let property in data) {
-            if (property.startsWith('_') || typeof data[property] === 'function') {
-                continue
-            }
-            console.log(property)
-            processedHtml = processedHtml.replace(new RegExp(`{{\\s*${property}\\s*}}`, 'g'), data[property])
-        }
-
-        let me = data._instanceId ? 'window.Loader.components[' + data._instanceId + ']' : 'me'
-        //replace occurrences of "me." with the `me` variable, but only if it's followed by a word boundary.
-        processedHtml = processedHtml.replace(new RegExp(`me\\.(\\w+)\\b`, 'g'), `${me}.$1`)
-
+    static loadHtml(html) {
         let template = document.createElement('template')
-        template.innerHTML = processedHtml
+        template.innerHTML = html
         return template.content.firstElementChild
-    }
-
-    static reloadComponent(Component) {
-        console.log('reloading component: ' + Component.constructor.name)
-        //todo: optimize this to only replace the parts that need changing somehow.
-        //todo: implement some kind of for x => y in y detection for repeating html tags.
-        let newElement = this.loadHtml(Component.html(), Component)
-        Component._element.replaceWith(newElement)
-        Component._element = newElement
     }
 
     static hashString(string) {
@@ -97,16 +47,10 @@ export default class Loader {
             if (['div', 'span', 'p', 'a', 'img', 'button', 'input', 'form', 'label', 'select', 'option', 'textarea', 'meta', 'head', 'script', 'link', 'html', 'body', 'title', 'style'].includes(tagName)) {
                 continue
             }
-            //skip regular HTML tags
-            if (
-                [
-                    'html', 'head', 'title', 'base', 'link', 'meta', 'style',
-                    'script', 'noscript', 'template', 'slot', 'img', 'picture', 'source', 'img', 'iframe', 'embed', 'object', 'param', 'video', 'audio', 'track', 'map', 'area', 'a', 'link', 'nav', 'ul', 'ol', 'li', 'dl', 'dt', 'dd', 'menu', 'menuitem', 'button', 'form', 'label', 'input', 'textarea', 'select', 'optgroup', 'option', 'fieldset', 'legend', 'datalist', 'output', 'progress', 'meter', 'details', 'summary', 'command', 'menu',
-                    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'footer', 'address', 'main', 'section', 'article', 'aside', 'nav', 'figure', 'figcaption', 'blockquote', 'div', 'p',
-                    'article', 'aside', 'details', 'figcaption', 'figure', 'footer', 'header', 'main', 'mark', 'nav', 'section', 'summary', 'time', 'audio', 'video', 'canvas', 'progress', 'meter', 'embed', 'object', 'param', 'iframe', 'picture', 'source', 'svg',
-                    'datalist', 'fieldset', 'legend', 'output', 'progress', 'meter', 'table', 'caption', 'colgroup', 'col', 'tbody', 'thead', 'tfoot', 'tr', 'td',
-                    'th', 'button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup', 'option', 'output', 'progress', 'select', 'textarea'
-                ].includes(tagName)) {
+            //skip all tag defined in the html 5 specification from w3c
+            if (['article', 'aside', 'details', 'figcaption', 'figure', 'footer', 'header', 'main', 'mark', 'nav', 'section', 'summary', 'time', 'audio', 'video', 'canvas', 'progress', 'meter', 'embed', 'object', 'param', 'iframe', 'picture', 'source', 'svg',
+                'datalist', 'fieldset', 'legend', 'output', 'progress', 'meter', 'table', 'caption', 'colgroup', 'col', 'tbody', 'thead', 'tfoot', 'tr', 'td',
+                'th', 'button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup', 'option', 'output', 'progress', 'select', 'textarea'].includes(tagName)) {
                 continue
             }
 
